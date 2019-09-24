66d3f52f62ac1fbbb1140bb6f06b6c825112d67e
Nathan Sorkin <natesrkn@gmail.com>
2019-09-24 12:32:04 -0400

    files added

---
+++ scripts/translations/get.js
@@ -0,0 +1,77 @@
+const inq = require('inquirer')
+const csv = require('csv-writer').createObjectCsvWriter
+
+const { constructV4ApiEndpoint } = require('../utils/api/apiHelpers')
+const { TRANSLATIONS_ENDPOINT } = require('../utils/api/endpoints')
+const api = require('../utils/api/callApi.js')
+
+const { clientDirectory } = require('../utils/helpers/csvHelpers.js')
+
+const questionPrompt = [
+  { type: 'input', name: 'filename', message: 'What will the filename be? Please include slash. Leave blank for default: "/translations.csv"' }
+]
+
+const init = (auth, data) => {
+  let directory,
+      filename
+
+  const defaultfilename = 'translations.csv'
+  inq.prompt(questionPrompt)
+    .then(async (answers) => {
+      filename = (answers.filename == '') ? defaultfilename : answers.filename
+      directory = clientDirectory(data.company, data.environment, filename)
+      const translations =  await getTranslations(auth)
+      return translations
+    })
+    .then(translations => {
+      formatTranslations(translations)
+    })
+    .catch(e => console.log(e))
+
+    const getTranslations = (auth) => {
+      const apiEndpoint = constructV4ApiEndpoint(data.environment, TRANSLATIONS_ENDPOINT)
+      let settings = {
+        url: apiEndpoint,
+        method: 'get',
+        headers: { 'Authorization' : 'Bearer ' + auth.token }
+      }
+      return api.call(apiEndpoint, settings)
+    }
+
+    const formatTranslations = (trns) => {
+      let translations = [];
+      trns.data.forEach(trn => {
+        let translation = {
+          type: trn.type,
+          id: trn.id,
+          locale: trn.attributes.locale,
+          key: trn.attributes.key,
+          value: trn.attributes.value,
+          resourceUri: trn.attributes.resourceUri
+        }
+        translations.push(translation)
+      })
+      printToCSV(translations)
+    }
+
+    const printToCSV = (data) => {
+      console.log(data)
+      const csvWriter = csv({
+        header: [
+          {id: 'type', title: 'type'},
+          {id: 'id', title: 'id'},
+          {id: 'locale', title: 'attributesLocale'},
+          {id: 'key', title: 'attributesKey'},
+          {id: 'value', title: 'attributesValue'},
+          {id: 'resourceUri', title: 'attributesResourceUri'}
+        ],
+        append: false,
+        path: directory
+      })
+      csvWriter.writeRecords(data).then(() => console.log('Done'))
+    }
+}
+
+module.exports = {
+  init,
+}
---
+++ scripts/translations/post.js
@@ -0,0 +1,80 @@
+const inq = require('inquirer')
+const api = require('../utils/api/callApi.js')
+const csv = require('csv-parser')
+const fs = require('fs')
+
+const { TRANSLATIONS_ENDPOINT } = require('../utils/api/endpoints.js')
+const { constructV4ApiEndpoint } = require('../utils/api/apiHelpers')
+const { clientDirectory } = require('../utils/helpers/csvHelpers.js')
+
+var filenameQuestionPrompt = [
+  { type: 'input', name: 'filename', message: 'What will the filename be? Please include slash. Leave blank for default: "/new_translations.csv"' }
+];
+
+const init = (auth, data) => {
+  let directory,
+      filename
+  
+  let defaultfilename = '/new_translations.csv'
+
+  inq.prompt(filenameQuestionPrompt)
+    .then(async(answers) => {
+      filename = (answers.filename == '') ? defaultfilename : answers.filename;
+      directory = clientDirectory(data.company, data.environment, filename)
+      readCsvFile(directory)
+    })
+    .catch (err => console.log(err))
+
+    const readCsvFile = (dir) => {
+      let translations = []
+      fs.createReadStream(dir)
+        .pipe(csv())
+        .on('data', (row) => {
+          try {
+            translations.push(row)
+          }
+          catch(err) {
+            console.log(err)
+          }
+        })
+        .on('end', () => {
+          parseCsvData(translations)
+        })
+    }
+
+    const parseCsvData = (trns) => {
+      trns.forEach((trn, index) => {
+        let translation = {
+          'data': {
+            'type': 'translation',
+            'attributes': {
+              'locale': trn.locale,
+              'key': trn.key,
+              'value': trn.value,
+              'resourceUri': trn.resourceUri
+            }
+          }
+        }
+
+        setTimeout(() => {
+          postTranslation(translation)
+        }, index * 1000)
+      })
+    }
+
+    const postTranslation = (trn) => {
+      console.log(trn)
+      const apiEndpoint = constructV4ApiEndpoint(data.environment, TRANSLATIONS_ENDPOINT)
+      let settings = {
+        method: 'post',
+        data: trn,
+        headers: { 'Authorization': 'Bearer ' + auth.token, },
+      }
+
+      return api.call(apiEndpoint, settings)
+    }
+}
+
+module.exports = {
+  init,
+}
